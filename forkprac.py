3aae1afad0fe1b4def219a98dcbadf824cb6b04d
rentalarisheeth <rentalarisheeth@gmail.com>
2026-08-02 23:34:01 +0530

    Patch accepted

--- forkprac.py
+++ forkprac.py
@@ -5,3 +5,5 @@ print("Succesfully your request merged")
 
 print("Hey man here i added new info about git patch command")
 print("SO basically a git patch command creates a text file which contains differences betwen current working file and previous commit version")
+
+print("Hey man i man testing git patch command okayy okayy hello risheeth")
