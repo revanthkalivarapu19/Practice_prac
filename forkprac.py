33ce14b49f8ea731dc87b4a6bc8899be3aca4058
Revanth Kalivarapu <revanth.k19112006@gmail.com>
2026-08-02 23:07:50 +0530

    git patch practice

--- forkprac.py
+++ forkprac.py
@@ -2,3 +2,6 @@ print("Hello from revanth")
 print("Hello from risheeth")
 print("New feature added trying to compare merge conflict")
 print("Succesfully your request merged")
+
+print("Hey man here i added new info about git patch command")
+print("SO basically a git patch command creates a text file which contains differences betwen current working file and previous commit version")
